a83b88541374e6ddafc28429c687ff064f68109b
Hardik Shah <hardik@praxinfo.com>
2017-10-03 17:47:21 +0530

    no message

--- HKKit/Example/TableViewController.swift
+++ HKKit/Example/TableViewController.swift
@@ -14,7 +14,7 @@ class TableViewController: UITableViewController {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+                 self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
