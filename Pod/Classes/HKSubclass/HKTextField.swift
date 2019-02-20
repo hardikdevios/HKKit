c904edc8043d522472ffce0b69b5d6e483c5d5d0
Hardik Shah <hardikrocks111@gmail.com>
2019-02-21 00:34:34 +0530

    - fix de init issue for weak delegate

--- Pod/Classes/HKSubclass/HKTextField.swift
+++ Pod/Classes/HKSubclass/HKTextField.swift
@@ -289,7 +289,7 @@ open class HKTextFieldDelegate:NSObject,UITextFieldDelegate {
     var textEditingStartClouser:cl_textfield?
     var textEditingStopClouser:cl_textfield?
 
-    var delegate:UITextFieldDelegate?
+    weak var delegate:UITextFieldDelegate?
     
     open func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
