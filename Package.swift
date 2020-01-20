5f4a2656df0a7431dc55eb6996afd3d7be07a8ed
Hardik Shah <hardikrocks111@gmail.com>
2020-01-20 19:31:14 +0530

    - Updated SPM Error

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .target(
             name: "HKKit",
-            path: "HKKit/Pod/Classes"),
+            path: "Pod/Classes"),
     
     ],
     swiftLanguageVersions: [.v5]
