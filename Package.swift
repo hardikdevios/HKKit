e186b040922c958a4eed8a45b7e4a519a7ba8185
Hardik Shah <hardikrocks111@gmail.com>
2020-01-20 19:26:48 +0530

    - SPM Support

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HKKit",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "HKKit",
+            targets: ["HKKit"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "HKKit",
+            path: "HKKit/Pod/Classes"),
+    
+    ],
+    swiftLanguageVersions: [.v5]
+
+)
+
