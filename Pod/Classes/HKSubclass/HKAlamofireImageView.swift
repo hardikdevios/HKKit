e91c8e34e910610e95ba78c9efd1ca8601a03ed4
Hardik Shah <hardikrocks111@gmail.com>
2021-11-11 13:07:24 +0530

    - remove Alamofire imageview

--- Pod/Classes/HKSubclass/HKAlamofireImageView.swift
+++ Pod/Classes/HKSubclass/HKAlamofireImageView.swift
@@ -12,47 +12,47 @@ import Alamofire
 import AlamofireImage
 open class HKAlamofireImageView: UIImageView {
     
-    private var recipt:RequestReceipt?
-    static var downloader = ImageDownloader(
-                configuration: ImageDownloader.defaultURLSessionConfiguration(),
-                downloadPrioritization: .lifo,
-                maximumActiveDownloads: 4,
-                imageCache: AutoPurgingImageCache()
-            )
+//    private var recipt:RequestReceipt?
+//    static var downloader = ImageDownloader(
+//                configuration: ImageDownloader.defaultURLSessionConfiguration(),
+//                downloadPrioritization: .lifo,
+//                maximumActiveDownloads: 4,
+//                imageCache: AutoPurgingImageCache()
+//            )
+//    
+//    public func cancelRequest(){
+//        guard let request = recipt else {
+//            return
+//        }
+//        HKAlamofireImageView.downloader.cancelRequest(with: request)
+//        
+//    }
     
-    public func cancelRequest(){
-        guard let request = recipt else {
-            return
-        }
-        HKAlamofireImageView.downloader.cancelRequest(with: request)
-        
-    }
-    
-    public func downloadImage(url:URL?,placeHolder:UIImage,imageType:HKAlamofireImageType = .normal,clouser:(()->())? = nil) {
-        self.image = placeHolder
-        guard let url = url else {
-            clouser?()
-            return
-        }
-        
-        self.recipt = HKAlamofireImageView.downloader.download(URLRequest(url:url), completion: { (response) in
-            
-            
-            if let image = response.result.value {
-                
-                if imageType == .rounded {
-                    self.image = image.af_imageRoundedIntoCircle()
-                }else{
-                    self.image = image
-                }
-                
-           
-            }
-            clouser?()
-            
-        })
-        
-    }
+//    public func downloadImage(url:URL?,placeHolder:UIImage,imageType:HKAlamofireImageType = .normal,clouser:(()->())? = nil) {
+//        self.image = placeHolder
+//        guard let url = url else {
+//            clouser?()
+//            return
+//        }
+//
+//        self.recipt = HKAlamofireImageView.downloader.download(URLRequest(url:url), completion: { (response) in
+//
+//
+//            if let image = response.result.value {
+//
+//                if imageType == .rounded {
+//                    self.image = image.af_imageRoundedIntoCircle()
+//                }else{
+//                    self.image = image
+//                }
+//
+//
+//            }
+//            clouser?()
+//
+//        })
+//
+//    }
     
     
     
