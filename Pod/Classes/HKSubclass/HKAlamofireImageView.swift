836c97e498104c16f8a33cdff7907cf1149606a4
Hardik Shah <hardikrocks111@gmail.com>
2023-04-02 20:57:00 +0530

    - remove alamofire imageview

--- Pod/Classes/HKSubclass/HKAlamofireImageView.swift
+++ Pod/Classes/HKSubclass/HKAlamofireImageView.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 #if canImport(AlamofireImage)
-import Alamofire
-import AlamofireImage
 open class HKAlamofireImageView: UIImageView {
     
 //    private var recipt:RequestReceipt?
